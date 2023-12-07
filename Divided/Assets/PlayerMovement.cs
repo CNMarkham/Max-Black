9c56fcfb1a092194d9ee5ffb6b8e972acf184651
Hayden <markhamonca@codeninjas.com>
2023-12-06 20:00:49 -0500

    njhhh

--- Divided/Assets/PlayerMovement.cs
+++ Divided/Assets/PlayerMovement.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-	public float speed2 = 2.0f;
+	public float speed = 6f;
+	public float speed2 = 3.0f;
 	public GameObject character;
 	public bool isGrounded;
 	public float JumpHold;
+	public LayerMask lPayerMask;
     void Update()
 	{
 
-		if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.C))
+        {
+
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		
+
+			if (Input.GetKey(KeyCode.RightArrow))
 		{
 			transform.position += Vector3.right * speed2 * Time.deltaTime;
 		}
@@ -24,9 +33,9 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.Z))
         {
 			JumpHold  += 0.1f;
-			if(JumpHold >= 10f)
+			if(JumpHold >= 6f)
             {	
-				JumpHold = 10f;
+				JumpHold = 6f;
             }
 
 		
@@ -36,8 +45,20 @@ public class PlayerMovement : MonoBehaviour
 		{
 			GetComponent<Animator>().SetBool("To Air or Not To Air? ", true);
 			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, JumpHold * 50));
+			isGrounded = false;
 			JumpHold = 0f;
 			
+		}
+
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, 2.33f, lPayerMask);
+		if (hit.collider != null)
+		{
+			isGrounded = true;
+			GetComponent<Animator>().SetBool("To Air or Not To Air? ", false);
+        }
+        else
+        {
+			GetComponent<Animator>().SetBool("To Air or Not To Air? ", true);
 		}
 	}
 }
