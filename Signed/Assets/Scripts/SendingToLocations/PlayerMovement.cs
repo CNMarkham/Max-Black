eeebe4e4c47d5f7a0b16fb1b3bfd02911c34c0f9
Chelsea <markhamonca@codeninjas.com>
2024-03-13 19:58:39 -0400

    asgf

--- Signed/Assets/Scripts/SendingToLocations/PlayerMovement.cs
+++ Signed/Assets/Scripts/SendingToLocations/PlayerMovement.cs
@@ -5,28 +5,33 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 	[Header("Speeds")]
-	public float Speed = 6f;
-	public float Speed2 = 3.0f;
-	
+	public float Speed = 6.5f;
+
 	[Header("Jumping Tools")]
+	public int JumpsLeft;
 	public bool isGrounded;
 	public float JumpHold;
 
 	[Header("LayerMask")]
 	public LayerMask lPayerMask;
-	void Update()
+
+    public void Start()
+    {
+		JumpsLeft = 2;
+    }
+    void Update()
 	{
 
 			if (Input.GetKey(KeyCode.RightArrow))
 		{
-			transform.position += Vector3.right * Speed2 * Time.deltaTime;
+			transform.position += Vector3.right * Speed * Time.deltaTime;
 		}
 		if (Input.GetKey(KeyCode.LeftArrow))
 		{
-			transform.position += Vector3.left * Speed2 * Time.deltaTime;
+			transform.position += Vector3.left * Speed * Time.deltaTime;
 
 		}
-		GetComponent<Animator>().SetFloat("Hinput", Input.GetAxis("Horizontal"));
+		GetComponent<Animator>().SetFloat("H-Input", Input.GetAxis("Horizontal"));
         if (Input.GetKey(KeyCode.Z))
         {
 			JumpHold  += 0.1f;
@@ -38,29 +43,29 @@ public class PlayerMovement : MonoBehaviour
 		
 
 		}
-		if (Input.GetKeyUp(KeyCode.Z) && isGrounded == true)
-		{
-			isGrounded = false;
-			GetComponent<Animator>().SetBool("To Air or Not To Air? ", true);
-			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, JumpHold * 50));
-            if (CompareTag("Floor"))
-            {
-				isGrounded = true;
-            }
-			JumpHold = 0f;
-			
-		}
 
-		RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, 2.33f, lPayerMask);
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, 1, lPayerMask);
 		if (hit.collider != null)
 		{
 			isGrounded = true;
-			GetComponent<Animator>().SetBool("To Air or Not To Air? ", false);
-        }
-        else
-        {
-			GetComponent<Animator>().SetBool("To Air or Not To Air? ", true);
+			JumpsLeft = 2;
+			//GetComponent<Animator>().SetTrigger("TriggerJump");
 		}
+		else
+		{
+			//GetComponent<Animator>().SetTrigger("TriggerJump");
+		}
+		if (Input.GetKeyUp(KeyCode.Z) && isGrounded == true && JumpsLeft >= 1)
+		{
+			isGrounded = false;
+			JumpsLeft -= 1;
+			GetComponent<Animator>().SetTrigger("TriggerJump");
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, JumpHold * 50));
+			JumpHold = 1f;
+			
+		}
+
+		
 	}
 }
 
